b0c8ad09e74be56f9a0dcecb3dde7ceb52878b07
bruce <350108081@qq.com>
2016-09-22 19:32:03 +0800

    支持win start.bat脚本启动(chrome version:46-50)

--- src/main/java/com/us/Main.java
+++ src/main/java/com/us/Main.java
@@ -1,10 +1,7 @@
 package com.us;
 
-import com.us.runtest.HomePageTest;
 import com.us.runtest.service.PublishServiceTest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
